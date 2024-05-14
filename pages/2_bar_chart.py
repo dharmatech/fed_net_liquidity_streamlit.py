3b6ab8066ea131e04995bf6622a17462f1829a32
dharmatech <wayo.cavazos@gmail.com>
2024-05-14 11:46:55 -0700

    2_bar_chart.py

---
+++ pages/2_bar_chart.py
@@ -0,0 +1,16 @@
+import datetime
+import plotly.express
+import plotly.graph_objects
+import fed_net_liquidity
+import streamlit as st
+import plotly
+
+@st.cache_data
+def get_dataframe():
+    return fed_net_liquidity.load_dataframe()
+
+df = get_dataframe()
+
+fig = plotly.express.bar(df, x='date', y=['WALCL', 'NL', 'RRP', 'TGA', 'REM'], title='Fed Net Liquidity', barmode='overlay')
+
+st.plotly_chart(fig)
