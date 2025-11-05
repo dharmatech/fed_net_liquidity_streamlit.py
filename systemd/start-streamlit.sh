fe3d5a170a528c33e8bbe3fd443ce8cfbb7add88
Ed Cavazos <wayo.cavazos@gmail.com>
2025-11-05 01:06:03 -0800

    start-streamlit.sh update

--- systemd/start-streamlit.sh
+++ systemd/start-streamlit.sh
@@ -1,32 +1,19 @@
 #!/usr/bin/bash
 
-. /home/dharmatech/.bashrc # for FRED_API_KEY
+# . /home/dharmatech/.bashrc # for FRED_API_KEY
+
+. /home/dharmatech/keys    # for FRED_API_KEY
 
 . /home/dharmatech/python-environments/env-3.10-streamlit/bin/activate
 
+export PYTHONPATH=$PYTHONPATH:/home/dharmatech/python-libs/yfinance_download.py
+
 cd /var/www/dharmatech.dev/data/temp/fed_net_liquidity_streamlit.py
 
-# streamlit run Fed_Net_Liquidity.py
-
-#streamlit run \
-#	--browser.serverAddress dharmatech.dev \
-#	--browser.serverPort 8501 \
-#	--server.sslCertFile /home/dharmatech/fullchain.pem \
-#	--server.sslKeyFile /home/dharmatech/privkey.pem \
-#	--server.baseUrlPath fed-net-liquidity \
-#	Fed_Net_Liquidity.py
-
 streamlit run \
-        --browser.serverPort 8501 \
-        --server.sslCertFile /home/dharmatech/fullchain.pem \
-        --server.sslKeyFile /home/dharmatech/privkey.pem \
-	--server.baseUrlPath fed-net-liquidity \
+        --server.address 127.0.0.1 \
+        --server.port 8501 \
+        --server.baseUrlPath fed-net-liquidity \
 	--server.enableCORS false \
+	--server.enableXsrfProtection false \
         Fed_Net_Liquidity.py
-
-
-
-# streamlit run \
-#        --browser.serverPort 8501 \
-#        --server.baseUrlPath fed-net-liquidity \
-#        Fed_Net_Liquidity.py
